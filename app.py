61f80964a1cc6674c86cecbc9ff886781bac0bf3
GiacomoPugliese <puglieseg24@gmail.com>
2024-05-09 01:39:51 -0400

    try

--- app.py
+++ app.py
@@ -408,9 +408,9 @@ try:
         st.session_state['begin_auth'] = True
         # Request OAuth URL from the FastAPI backend
         url = f"{'https://leadership-initiatives-0c372bea22f2.herokuapp.com'}/auth?user_id={collection_id}"
-        st.write(url)
+
         response = requests.get(url)
-        st.write(response)
+
         if response.status_code == 200:
             # Get the authorization URL from the response
             auth_url = response.json().get('authorization_url')
